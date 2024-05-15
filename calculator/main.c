9c6c21907e90bfad01c429cd9e9f048c59519dc3
Hitesh <hiteshtheviper9@gmail.com>
2024-05-16 05:15:30 +0530

    modified main compiled successfully

--- calculator/main.c
+++ calculator/main.c
@@ -10,27 +10,27 @@ int main()
     printf("Enter a number: ");
     scanf("%f",&num1);
     printf("(+) ADD\t\t(-) SUBTRACT\t\t(*) MULTIPLY\t\t(/) DIVISION\n");
-    scanf("%c",&options);
+    scanf("\n%c", &options);
     switch (options) {
     case '+' :
         printf("Enter a number: ");
         scanf("%f",&num2);
-        printf("After addition the value is : %0.2f\n", num1 + num2);
+        printf("Value is : %0.2f\n", num1 + num2);
         break;
     case '-':
         printf("Enter a number: ");
         scanf("%f",&num2);
-        printf("After subtraction  the value is : %0.2f\n", num1 - num2);
+        printf("Value is : %0.2f\n", num1 - num2);
         break;
     case '*':
         printf("Enter a number: ");
         scanf("%f",&num2);
-        printf("After multiplication the value is : %0.2f\n", num1 * num2);
+        printf("Value is : %0.2f\n", num1 * num2);
         break;
     case '/':
         printf("Enter a number: ");
         scanf("%f",&num2);
-        printf("After division the value is : %0.2f\n", num1 / num2);
+        printf("Value is : %0.2f\n", num1 / num2);
         break;
     default:
         printf("Invalid options\n");
