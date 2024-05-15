4f1c606d4b9f374d457f2f97211b291852b057bd
Hitesh <hiteshtheviper9@gmail.com>
2024-05-16 04:38:04 +0530

    modified main.c

--- calculator/main.c
+++ calculator/main.c
@@ -5,27 +5,27 @@
 int main()
 {
     float num1, num2;
-    int options;
+    char options;
     printf("\t\t***CALCULATOR***\n");
-    printf("Select options to perform operations\n(1) ADD\n(2) SUBTRACT\n(3) MULTIPLY\n(4) DIVISION\n");
-    scanf("%d",&options);
+    printf("Select options to perform operations\n(+) ADD\n(-) SUBTRACT\n(*) MULTIPLY\n(/) DIVISION\n");
+    scanf("%c",&options);
     switch (options) {
-    case 1:
+    case '+':
         printf("Enter two numbers: ");
         scanf("%f%f",&num1,&num2);
         printf("After addition the value is : %0.2f\n", num1 + num2);
         break;
-    case 2:
+    case '-':
         printf("Enter two numbers: ");
         scanf("%f%f",&num1,&num2);
         printf("After subtraction  the value is : %0.2f\n", num1 - num2);
         break;
-    case 3:
+    case '*':
         printf("Enter two numbers: ");
         scanf("%f%f",&num1,&num2);
         printf("After multiplication the value is : %0.2f\n", num1 * num2);
         break;
-    case 4:
+    case '/':
         printf("Enter two numbers: ");
         scanf("%f%f",&num1,&num2);
         printf("After division the value is : %0.2f\n", num1 / num2);
